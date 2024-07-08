166863580972a00047a39ab20f404509ee573188
arshdeepsingh <arshdeepsingh@ARSHDEEPSINA1B3>
2024-07-08 13:33:11 -0700

    Add project files.

---
+++ StudentManagement/Models/SchoolContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace StudentManagement.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
